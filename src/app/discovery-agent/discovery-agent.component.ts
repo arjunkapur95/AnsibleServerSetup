98825845e8e4e136a68039309848219b7714e7df
arjun <arjunkapur95@gmail.com>
2018-07-01 16:39:32 +1000

    discovery agent

--- src/app/discovery-agent/discovery-agent.component.ts
+++ src/app/discovery-agent/discovery-agent.component.ts
@@ -8,18 +8,42 @@ import { Component, OnInit } from '@angular/core';
 export class DiscoveryAgentComponent implements OnInit {
 
   private fileText;
+  private fileText1;
+  private fileText2;
  
   constructor() { }
  
   ngOnInit() { }
  
     fileUpload(event) {
+	console.log("fileupload");
     var reader = new FileReader();
     reader.readAsText(event.srcElement.files[0]);
     var me = this;
     reader.onload = function () {
       me.fileText = reader.result;
     }
-  }
+	
+	}
+	fileUpload1(event) {
+	console.log("fileupload1");
+    var reader1 = new FileReader();
+    reader1.readAsText(event.srcElement.files[0]);
+    var me = this;
+    reader1.onload = function () {
+      me.fileText1 = reader1.result;
+    }
+	
+	}
+	fileUpload2(event) {
+	console.log("fileupload2");
+    var reader2 = new FileReader();
+    reader2.readAsText(event.srcElement.files[0]);
+    var me = this;
+    reader2.onload = function () {
+      me.fileText2 = reader2.result;
+    }
+	
+	}
 
 }
